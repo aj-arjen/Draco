9b3e4876cb3953d2ad9325d915496c8389f8e13a
aj-arjen <aj-arjen@ziggo.nl>
2026-07-12 15:13:11 +0200

    Add files via upload

--- views.py
+++ views.py
@@ -30,10 +30,10 @@ class LanguageView(discord.ui.View):
         interaction: discord.Interaction,
         select: discord.ui.Select
     ):
-    await interaction.response.edit_message(
-    content="🏰 Select your guild:",
-    view=GuildView(select.values[0])
-)
+        await interaction.response.edit_message(
+            content="🏰 Select your guild:",
+            view=GuildView(select.values[0])
+        )
 
 
 class GuildView(discord.ui.View):
