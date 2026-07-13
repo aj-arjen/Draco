afe2548250513b97191ca0146726361695c41ae7
aj-arjen <aj-arjen@ziggo.nl>
2026-07-13 20:33:00 +0200

    Add files via upload

---
+++ giftcode_watcher.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import tasks
+
+class GiftCodeWatcher:
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.last_code = None
+
+    @tasks.loop(minutes=30)
+    async def check_giftcodes(self):
+
+        print("🔍 Checking for new gift codes...")
+
+    @check_giftcodes.before_loop
+    async def before_check(self):
+
+        await self.bot.wait_until_ready()
+
+    def start(self):
+
+        self.check_giftcodes.start()
