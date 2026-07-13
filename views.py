d28efe5744d834194a5700164df906b04257ce23
aj-arjen <aj-arjen@ziggo.nl>
2026-07-13 18:08:55 +0200

    Add files via upload

--- views.py
+++ views.py
@@ -303,42 +303,42 @@ class ReviewView(discord.ui.View):
 
     if log_channel:
 
-    log_embed = discord.Embed(
-        title="🟢 Application Accepted",
-        color=discord.Color.green()
-    )
+        log_embed = discord.Embed(
+            title="🟢 Application Accepted",
+            color=discord.Color.green()
+       )
 
-    log_embed.add_field(
-        name="👤 Applicant",
-        value=member.mention,
-        inline=False
-    )
+        log_embed.add_field(
+            name="👤 Applicant",
+            value=member.mention,
+            inline=False
+        )
 
-    log_embed.add_field(
-        name="🎮 In-game Name",
-        value=self.ign,
-        inline=True
-    )
+        log_embed.add_field(
+            name="🎮 In-game Name",
+            value=self.ign,
+            inline=True
+        )
 
-    log_embed.add_field(
-        name="🏰 Guild",
-        value=self.guild,
-        inline=True
-    )
+        log_embed.add_field(
+            name="🏰 Guild",
+            value=self.guild,
+            inline=True
+        )
 
-    log_embed.add_field(
-        name="⭐ Rank",
-        value=self.rank.title(),
-        inline=True
-    )
+        log_embed.add_field(
+            name="⭐ Rank",
+            value=self.rank.title(),
+            inline=True
+        )
 
-    log_embed.add_field(
-        name="👮 Approved by",
-        value=interaction.user.mention,
-        inline=False
-    )
+        log_embed.add_field(
+            name="👮 Approved by",
+            value=interaction.user.mention,
+            inline=False
+        )
 
-    log_embed.timestamp = discord.utils.utcnow()
+        log_embed.timestamp = discord.utils.utcnow()
 
     await log_channel.send(embed=log_embed)
 
