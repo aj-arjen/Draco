ced08098a1a0eeed157dd37e16ddd1e6cfe8e8f1
aj-arjen <aj-arjen@ziggo.nl>
2026-07-13 21:10:48 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import os
 from bot import Draco
 from settings import TOKEN
 from views import ApplyView
-from giftcodes import GiftCodeView
+from giftcodes import post_giftcode
 from giftcode_watcher import GiftCodeWatcher
 
 bot = Draco()
@@ -44,30 +44,9 @@ async def gifttest(interaction: discord.Interaction):
 
     code = "DRACO2026"
 
-
-    embed = discord.Embed(
-        title="🎁 New Top Heroes Gift Code",
-        color=0xC49A3A
-    )
-
-    embed.description = (
-        "A new gift code is available!\n\n"
-        f"```{code}```"
-    )
-
-    embed.add_field(
-        name="Status",
-        value="🟢 Active",
-        inline=True
-    )
-
-    embed.set_footer(
-        text="Powered by Draco 🐉"
-    )
-
-    await interaction.channel.send(
-        embed=embed,
-        view=GiftCodeView(code)
+    await post_giftcode(
+        interaction.channel,
+        code
     )
 
     await interaction.response.send_message(
