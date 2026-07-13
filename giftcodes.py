616b40040892ceea5f3be9899ef541e92088f21d
aj-arjen <aj-arjen@ziggo.nl>
2026-07-13 21:21:25 +0200

    Add files via upload

--- giftcodes.py
+++ giftcodes.py
@@ -1,13 +1,12 @@
 import discord
 
+
 REDEEM_URL = "https://topheroes.store.kopglobal.com/en"
 
 
 class GiftCodeView(discord.ui.View):
     def __init__(self, code):
         super().__init__(timeout=None)
-
-        self.code = code
 
         redeem = discord.ui.Button(
             label="Redeem",
@@ -16,23 +15,6 @@ class GiftCodeView(discord.ui.View):
         )
 
         self.add_item(redeem)
-
-    @discord.ui.button(
-        label="Copy Code",
-        emoji="📋",
-        style=discord.ButtonStyle.blurple,
-        custom_id="copy_giftcode"
-    )
-    async def copy_code(
-        self,
-        interaction: discord.Interaction,
-        button: discord.ui.Button
-    ):
-
-        await interaction.response.send_message(
-            f"📋 Copy this code:\n```{self.code}```",
-            ephemeral=True
-        )
 
 
 async def post_giftcode(channel, code):
