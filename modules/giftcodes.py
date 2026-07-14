3e65f366cc87068f50034538ec72fd92c70ed91b
aj-arjen <aj-arjen@ziggo.nl>
2026-07-14 20:21:17 +0200

    Add files via upload

---
+++ modules/giftcodes.py
@@ -0,0 +1,45 @@
+import discord
+
+
+REDEEM_URL = "https://topheroes.store.kopglobal.com/en"
+
+
+class GiftCodeView(discord.ui.View):
+    def __init__(self, code):
+        super().__init__(timeout=None)
+
+        redeem = discord.ui.Button(
+            label="Redeem",
+            emoji="🌐",
+            url=REDEEM_URL
+        )
+
+        self.add_item(redeem)
+
+
+async def post_giftcode(channel, code):
+
+    embed = discord.Embed(
+        title="🎁 New Top Heroes Gift Code",
+        color=0xC49A3A
+    )
+
+    embed.description = (
+        "A new gift code is available!\n\n"
+        f"```{code}```"
+    )
+
+    embed.add_field(
+        name="Status",
+        value="🟢 Active",
+        inline=True
+    )
+
+    embed.set_footer(
+        text="Powered by Draco 🐉"
+    )
+
+    await channel.send(
+        embed=embed,
+        view=GiftCodeView(code)
+    )
