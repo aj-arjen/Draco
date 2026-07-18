74f0afefbd38778c8f101648b287d3238f979d1b
aj-arjen <aj-arjen@ziggo.nl>
2026-07-18 14:11:28 +0200

    Add files via upload

---
+++ config/draco_assets.py
@@ -0,0 +1,12 @@
+DRACO_IMAGES = {
+    "yes": "assets/draco/draco_yes.png",
+    "no": "assets/draco/draco_no.png",
+    "shy": "assets/draco/draco_shy.png",
+    "serious": "assets/draco/draco_serious.png",
+    "confused": "assets/draco/draco_confused.png",
+    "thinks": "assets/draco/draco_thinks.png",
+    "celebrate": "assets/draco/draco_celebrate.png",
+    "heart": "assets/draco/draco_heart.png",
+    "hype": "assets/draco/deaco_hype.png",
+    "cry": "assets/draco/draco_cry.png",
+}
