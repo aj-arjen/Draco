6137ccf5a13ffcfe57827070f8ab21bc0ff30e7f
aj-arjen <aj-arjen@ziggo.nl>
2026-07-19 15:29:41 +0200

    Add files via upload

--- modules/hero.py
+++ modules/hero.py
@@ -1,6 +1,9 @@
 from discord import app_commands
 from discord.ext import commands
 
+import json
+from pathlib import Path
+
 
 class Hero(commands.Cog):
     def __init__(self, bot):
@@ -11,8 +14,15 @@ class Hero(commands.Cog):
         description="View information about a hero."
     )
     async def hero(self, interaction):
+    hero_file = Path(
+        "hero_database/factions/league/heroes/legendary/adjudicator.json"
+    )
+
+        with open(hero_file, "r", encoding="utf-8") as f:
+            hero = json.load(f)
+
         await interaction.response.send_message(
-            "Hero command is working!"
+            hero["name"]
         )
 
 
