9958d0e03b798b82b9308980ce7681d5163d3dc2
aj-arjen <aj-arjen@ziggo.nl>
2026-07-21 21:01:34 +0200

    Add files via upload

--- views/relic_recommendation_views.py
+++ views/relic_recommendation_views.py
@@ -4,6 +4,7 @@ from discord.ui import View, Select
 from utils.relic_recommendations import RECOMMENDATIONS
 from utils.relic_loader import load_relic
 from utils.relic_embed import create_relic_embed
+from utils.relic_recommendation_embed import create_recommendation_embed
 
 class RecommendationMainView(View):
     def __init__(self):
@@ -40,3 +41,63 @@ class RecommendationSelect(Select):
             embed=embed,
             view=RecommendationView(faction)
         )
+class RecommendationView(View):
+    def __init__(self, faction: str, f2p: bool = False):
+        super().__init__(timeout=None)
+
+        self.faction = faction
+        self.f2p = f2p
+
+        if f2p:
+            self.add_item(BestSetButton(faction))
+        else:
+            self.add_item(F2PSetButton(faction))
+
+        self.add_item(BackButton())
+        
+class F2PSetButton(discord.ui.Button):
+    def __init__(self, faction: str):
+        super().__init__(
+            label="🆓 F2P Set",
+            style=discord.ButtonStyle.primary
+        )
+
+        self.faction = faction
+
+    async def callback(self, interaction: discord.Interaction):
+        embed = create_recommendation_embed(
+            self.faction,
+            f2p=True
+        )
+
+        await interaction.response.edit_message(
+            embed=embed,
+            view=RecommendationView(
+                self.faction,
+                f2p=True
+            )
+        )
+
+
+class BestSetButton(discord.ui.Button):
+    def __init__(self, faction: str):
+        super().__init__(
+            label="🏆 Best Set",
+            style=discord.ButtonStyle.success
+        )
+
+        self.faction = faction
+
+    async def callback(self, interaction: discord.Interaction):
+        pass
+
+
+class BackButton(discord.ui.Button):
+    def __init__(self):
+        super().__init__(
+            label="⬅ Back",
+            style=discord.ButtonStyle.secondary
+        )
+
+    async def callback(self, interaction: discord.Interaction):
+        pass
