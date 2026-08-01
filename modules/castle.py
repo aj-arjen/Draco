1e7879308994f7921b9059c1588f9f0502fadee5
aj-arjen <aj-arjen@ziggo.nl>
2026-08-01 04:09:14 +0200

    Add files via upload

--- modules/castle.py
+++ modules/castle.py
@@ -1,29 +1,28 @@
 import discord
-from discord import app_commands
 from discord.ext import commands
 
-from views.castle_views import CastleSelectView
+from views.castle_views import CastleMainView
 
 
 class Castle(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command(
+    @discord.app_commands.command(
         name="castle",
-        description="Browse all castle skins."
+        description="Browse castle skins"
     )
-    async def castle(self, interaction: discord.Interaction):
-
+    async def relic(self, interaction: discord.Interaction):
         embed = discord.Embed(
-            title="Castle Database",
-            description="Choose a castle skin from the dropdown below.",
+            title="🗿 Castle Skins",
+            description="Choose an option below.",
             color=discord.Color.gold()
         )
 
         await interaction.response.send_message(
             embed=embed,
-            view=CastleSelectView()
+            view=CastleMainView(),
+            ephemeral=False
         )
 
 
